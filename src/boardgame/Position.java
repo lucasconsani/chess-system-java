be208ddefc8ceb6722af88607331eea7b7b4d701
Lucas Consani <lucasconsani.lc@gmail.com>
2024-05-20 18:01:57 -0300

    created class board em piece e altered class position

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -13,4 +13,20 @@ public class Position {
     public String toString(){
         return row + ", " + column;
     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
 }
